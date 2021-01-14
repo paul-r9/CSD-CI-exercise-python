c961e9094d88fcfdfbb0be02e02b607a96df5ec8
Karis Petersen <petersen.kr1@gmail.com>
2021-01-14 15:57:28 -0700

    add SanitizerTest

--- test_isbn_sanitizer.py
+++ test_isbn_sanitizer.py
@@ -4,17 +4,15 @@ import unittest
 from isbn import ISBN
 
 
-class ISBNTest(unittest.TestCase):
-    def test_needs_a_better_name(self):
+class ISBNSanitizerTest(unittest.TestCase):
+    def test_string_sanitizer_removes_spaces(self):
         # Arrange
-        sut = ISBN()
- 
+        expected = "00201485672"
         # Act
-        result = sut.validate("9780470059029")
-
+        # result = sanitize("0 0201485672")
 
         # Assert
-        self.assertEqual(True, result)
+        # self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
