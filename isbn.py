cef6ed739a06a262bba3a2534d49eb649e96508f
Janaki Jeyabalan <janaki.jeyabalan@gatesfoundation.org>
2021-01-14 15:02:30 -0800

    valid/invalid test

--- isbn.py
+++ isbn.py
@@ -1,4 +1,10 @@
 class ISBN:
 
     def validate(self, number):
+        return self.checkSum(number)
+
+    def checkSum(self, number):
+        checkDigit = int(number[-1])
+        if not (checkDigit % 2):
+            return False
         return True
--- test_isbn.py
+++ test_isbn.py
@@ -5,7 +5,7 @@ from isbn import ISBN
 
 
 class ISBNTest(unittest.TestCase):
-    def test_needs_a_better_name(self):
+    def test_valid_isbn13(self):
         # Arrange
         sut = ISBN()
  
@@ -16,6 +16,16 @@ class ISBNTest(unittest.TestCase):
         # Assert
         self.assertEqual(True, result)
 
+    def test_invalid_isbn13(self):
+        # Arrange
+        sut = ISBN()
+ 
+        # Act
+        result = sut.validate("9780470059028")
+
+
+        # Assert
+        self.assertEqual(False, result)
 
 if __name__ == '__main__':
     unittest.main()
