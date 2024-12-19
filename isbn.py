5bd322463bf3406a2ca27700c0d9528103990bb3
Sylvia Waldron <sylvia.waldron@umanitoba.ca>
2024-12-19 14:11:56 -0600

    Perform ISBN-13 checksum

--- isbn.py
+++ isbn.py
@@ -1,4 +1,22 @@
+from functools import partial
+from itertools import chain
+from operator import mul
+
+
 class ISBN:
 
     def validate(self, number):
-        return True
+        if len(digits) != 13:
+            return False
+
+        checksum = digits.pop()
+        odd_indexes = digits[::2]
+        even_indexes = digits[1::2]
+
+        even_indexes_mul_3 = map(partial(mul, 3), even_indexes)
+
+        digit_sum = sum(chain(odd_indexes, even_indexes_mul_3))
+        digit_sum_mod_10 = digit_sum % 10
+        digit_sum_mod_10_subtracted_from_10 = 10 - digit_sum_mod_10
+
+        return (digit_sum_mod_10_subtracted_from_10 % 10) == checksum
