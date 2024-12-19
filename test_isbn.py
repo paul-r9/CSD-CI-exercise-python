d1111eaa586e5a6243da289b1afb5d1e04ac95b6
Sylvia Waldron <sylvia.waldron@umanitoba.ca>
2024-12-19 10:13:58 -0600

    Update test for pytest

--- test_isbn.py
+++ test_isbn.py
@@ -1,22 +1,12 @@
-# -*- coding: utf-8 -*-
-import unittest
-
 from isbn import ISBN
 
 
-class ISBNTest(unittest.TestCase):
+def test_valid_isbn13():
+    # Arrange
+    sut = ISBN()
 
-    def test_valid_isbn13(self):
-        # Arrange
-        sut = ISBN()
- 
-        # Act
-        result = sut.validate("9780470059029")
+    # Act
+    result = sut.validate("9780470059029")
 
-
-        # Assert
-        self.assertEqual(True, result)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    # Assert
+    assert result
