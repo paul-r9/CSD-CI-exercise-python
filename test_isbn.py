fec40dffdd57adaf90dd8a0a299ad280b017e1de
Sylvia Waldron <sylvia.waldron@umanitoba.ca>
2024-12-19 14:16:23 -0600

    Add isbn13 too short test

--- test_isbn.py
+++ test_isbn.py
@@ -12,7 +12,7 @@ def test_valid_isbn13():
     assert result
 
 
-def test_invalid_isbn13():
+def test_invalid_isbn13_checksum_wrong():
     # Arrange
     sut = ISBN()
 
@@ -20,4 +20,13 @@ def test_invalid_isbn13():
     result = sut.validate("9780470059028")
 
     # Assert
+    assert not result
+
+
+def test_invalid_isbn13_is_too_short():
+
+    sut = ISBN()
+
+    result = sut.validate("982")
+
     assert not result
