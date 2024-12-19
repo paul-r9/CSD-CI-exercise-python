94208d3607c478c48e024c76fde74f5cbdebc5f2
Sylvia Waldron <sylvia.waldron@umanitoba.ca>
2024-12-19 15:08:37 -0600

    Convert map to list comprehension

--- isbn.py
+++ isbn.py
@@ -1,6 +1,4 @@
-from functools import partial
 from itertools import chain
-from operator import mul
 
 from dataclasses import dataclass
 
@@ -24,7 +22,7 @@ class ISBN:
         odd_indexes = digits[::2]
         even_indexes = digits[1::2]
 
-        even_indexes_mul_3 = map(partial(mul, 3), even_indexes)
+        even_indexes_mul_3 = [3*digit for digit in even_indexes]
 
         digit_sum = sum(chain(odd_indexes, even_indexes_mul_3))
         digit_sum_mod_10 = digit_sum % 10
